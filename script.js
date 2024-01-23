494543aa5bf282ede90df179dbcab7e83a571af5
Ramil Aminov <roma_aminov99@list.ru>
2024-01-24 04:04:01 +0600

    sec lending v1.2

--- script.js
+++ script.js
@@ -4,13 +4,42 @@
 //     button.classList.toggle('clicked');
 //     console.log('rr');
 // });
-function sumTimesTables(tables, min, max){
-    let x=0;
-    for(let numbInt of tables){
-      for(let i=min;i<=max;i++){
-        x+=numbInt*i;
-      }
-    }
-    return x;
-  }
-console.log(sumTimesTables([2,5],1,3))
+// function sumTimesTables(tables, min, max){
+//     let x=0;
+//     for(let numbInt of tables){
+//       for(let i=min;i<=max;i++){
+//         x+=numbInt*i;
+//       }
+//     }
+//     return x;
+//   }
+// console.log(sumTimesTables([2,5],1,3))
+// const reverseSeq = n =>{
+//   let x=[];
+//   for(let i=n;i>0;i--){
+//     x.push(i)
+//   }
+//   return x
+// }
+// console.log(reverseSeq(5))
+// function repeatStr(n,s){
+//   return s.repeat(n)
+// }
+// console.log(repeatStr(5,'ramil'))
+// function averP(classP,me){
+//   return classP.reduce((acc,value)=>acc+value,0)/classP.length<me;
+  
+// }
+// console.log(averP([100,40,34,57,29,33,100,100,100],75))
+// function dg(n){
+//   return n.toString().split('').reverse().map(el=>+el);
+// }
+// console.log(dg(32144))
+// numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve","thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"]
+// function wallpaper(l, w, h) {
+//   let rollS=5.2;
+//   let wallS=2*(l*h)+2*(w*h);
+//   let total=Math.ceil(wallS/rollS +wallS/rollS*0.15);
+//   return numbers[total]
+//   }
+//   console.log(wallpaper(6.3, 5.8, 3.13))
